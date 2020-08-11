6a007501b1ab6f9c62d2b1545a1c7be8d15f24d8
Luis Mayta <luis.mayta@gmail.com>
2020-08-11 18:29:22 -0500

    creando los metodos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { HomeComponent } from './components/home/home.component';
+import { ProtegidaComponent } from './components/protegida/protegida.component';
+import { PreciosComponent } from './components/precios/precios.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    HomeComponent,
+    ProtegidaComponent,
+    PreciosComponent
   ],
   imports: [
     BrowserModule,
